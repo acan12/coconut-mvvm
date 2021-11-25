829187ff57781c2a7c4fa827315a98c88f70d33e
ary <ary@clappingape.com>
2021-11-25 12:49:52 +0700

    clean baseRepository

--- coconut-mvvm-code/src/main/java/app/beelabs/coconut/mvvm/base/BaseRepository.kt
+++ coconut-mvvm-code/src/main/java/app/beelabs/coconut/mvvm/base/BaseRepository.kt
@@ -4,7 +4,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.ResponseBody
 import retrofit2.HttpException
-import retrofit2.Response
 
 open class BaseRepository {
 
@@ -25,23 +24,6 @@ open class BaseRepository {
             }
         }
     }
-//    suspend fun <T> safeApiCall(apiCall: suspend () -> Response<T>): Resource<T> {
-//        try {
-//            val response = apiCall()
-//            if (response.isSuccessful) {
-//                val body = response.body()
-//                body?.let {
-//                    return Resource.Success(body)
-//                }
-//            }
-//            return error("${response.code()} ${response.message()}")
-//        } catch (e: Exception) {
-//            return error(e.message ?: e.toString())
-//        }
-//    }
-//
-//    private fun <T> error(errorMessage: String): Resource<T> =
-//        Resource.Error("Api call failed $errorMessage")
 }
 
 sealed class Resource<out T> {
