d2b3c9d797435356bd0b3e63963de2cd78d5b459
ary <ary@clappingape.com>
2021-12-09 13:17:47 +0700

    add base repository for smart data sources

--- coconut-mvvm-code/src/main/java/app/beelabs/coconut/mvvm/base/BaseRepository.kt
+++ coconut-mvvm-code/src/main/java/app/beelabs/coconut/mvvm/base/BaseRepository.kt
@@ -1,11 +1,26 @@
 package app.beelabs.coconut.mvvm.base
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.withContext
 import okhttp3.ResponseBody
 import retrofit2.HttpException
 
 open class BaseRepository {
+
+//    fun <T> resultFlow(
+//        localLoad: suspend () -> Resource<T>,
+//        networkCall: suspend () -> Resource<T>,
+//        saveCallResult: suspend (T) -> Unit
+//    ) = flow<Resource<T>> {
+//        emit(Resource.Loading<T>())
+//        val data = (localLoad() as Resource.Success<T>).value
+//        when {
+//            data != null -> checkDataType(data, networkCall, saveCallResult)
+//            else -> emit(callRemote(networkCall, saveCallResult))
+//        }
+//    }.flowOn(Dispatchers.IO)
 
     suspend fun <T> safeApiCall(apiCall: suspend () -> T): Resource<T> {
         return withContext(Dispatchers.IO) {
